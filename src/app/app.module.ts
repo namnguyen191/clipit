606094e17f6aec0a31e8b8c1d2868b1742181c8d
NAM NGUYEN <hoangnamnguyen191@gmail.com>
2022-04-21 20:43:11 -0400

    feat: add angular fire and build script

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { BrowserModule } from '@angular/platform-browser';
+import { environment } from 'src/environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [AppComponent, NavComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAcBedrPQWtwsBA-t8ZSjUk3y4SQ8eGRK8',
+    authDomain: 'clipit-31d1c.firebaseapp.com',
+    projectId: 'clipit-31d1c',
+    storageBucket: 'clipit-31d1c.appspot.com',
+    messagingSenderId: '169059670918',
+    appId: '1:169059670918:web:0da8e64eae1eddd785ea07'
+  }
 };
 
 /*
